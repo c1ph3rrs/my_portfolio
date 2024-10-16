eec870f469b1280a4cb068d500d9678faca014ae
Mohammed Suyel <ciphers.developers@gmail.com>
2024-10-16 15:46:52 +0400

    some more changes added

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -24,9 +24,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7b#7hc@ncm(s^p=sw$bk^t%6aenmhuuni$59q79cdl!et%!4t+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ib-mughal.com', 'www.ib-mughal.com']
 
 
 # Application definition
