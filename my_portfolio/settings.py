90ddb4b86b8201bbeebea0426d6e8466f2b8a57a
Ibrahim Akram <68148781+c1ph3rrs@users.noreply.github.com>
2024-10-20 00:04:26 +0400

    some change added

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7b#7hc@ncm(s^p=sw$bk^t%6aenmhuuni$59q79cdl!et%!4t+'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
